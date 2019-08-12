9a9a438f7b9d8d14348dcc52219e568c7f34e2a2
Zhaoyu Luo <luozhaoyu90@gmail.com>
2019-08-12 18:55:09 -0400

    comment

--- form_combination/form_combination.py
+++ form_combination/form_combination.py
@@ -14,6 +14,7 @@ def form_combinations():
     header_a, list_a = read_list_from_file("a.txt")
     header_b, list_b = read_list_from_file("b.txt")
     header_c, list_c = read_list_from_file("c.txt")
+    # output_file_name 是输出文件路径，默认是当前目录下，也可改成 C:\output.csv
     output_file_name = "output_{}_{}_{}.csv".format(header_a, header_b, header_c)
     with open(output_file_name, "w") as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
