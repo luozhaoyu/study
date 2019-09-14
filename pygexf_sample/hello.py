dacd4e0b5f983773a4aa86f749b52250386ac49f
Zhaoyu Luo <luozhaoyu90@gmail.com>
2019-09-13 23:51:31 -0400

    add hello.py for pygexf

---
+++ pygexf_sample/hello.py
@@ -0,0 +1,12 @@
+from gexf import Gexf
+
+# test helloworld.gexf
+gexf = Gexf("Paul Girard", "A hello world! file")
+graph = gexf.addGraph("directed", "static", "a hello world graph")
+
+graph.addNode("0", "hello")
+graph.addNode("1", "World")
+graph.addEdge("0", "0", "1")
+
+output_file = open("helloworld.gexf", "w")
+gexf.write(output_file)
